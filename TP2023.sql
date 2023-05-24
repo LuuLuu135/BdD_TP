4152281842cea9bf9e04bf993ffcb298f6334354
LuuLuu135 <107959581+LuuLuu135@users.noreply.github.com>
2023-05-24 14:09:11 -0300

    Add files via upload

--- TP2023.sql
+++ TP2023.sql
@@ -10,16 +10,35 @@ Nacionalidad varchar(30)
 );
 
 -- INSERT INTO JUGADOR VALUES ( );
+SELECT *FROM JUGADOR;
 
+CREATE TABLE MAPA(
 
+ID_Mapa int IDENTITY PRIMARY KEY, 
+Nombre_Mapa varchar(30)
+);
+
+--INSERT INTO MAPA VALUES( );
+SELECT *FROM MAPA;
+
+CREATE TABLE MODALIDAD(
+
+ID_Modalidad int IDENTITY PRIMARY KEY,
+Nombre_Modalidad varchar(30)
+);
+
+--INSERT INTO MODALIDAD VALUES( );
 
 CREATE TABLE PARTIDA(
 
-ID_Partida int PRIMARY KEY NOT NULL,
+ID_Partida int PRIMARY KEY NOT NULL, --6 digitos
 Hora_in varchar(5),
-Duracion varchar(10),
-Mapa varchar(20),
-Modo varchar(20)
+Duracion varchar(10), --en minutos
+ID_Mapa int,
+ID_Modalidad int
+
+FOREIGN KEY (ID_Mapa) REFERENCES MAPA(ID_Mapa),
+FOREIGN KEY (ID_Modalidad) REFERENCES MODALIDAD(ID_Modalidad)
 
 );
 
@@ -28,8 +47,10 @@ Modo varchar(20)
 CREATE TABLE SESION(
 
 ID_Sesion int IDENTITY PRIMARY KEY NOT NULL,
-Fecha_hr_in varchar(20),
-Fecha_hr_eg Varchar(20),
+Fecha_in varchar(10),
+Hora_in varchar(10),
+Fecha_eg Varchar(10),
+Hora_eg varchar(10),
 Nickname varchar(30)
 
 FOREIGN KEY (Nickname) REFERENCES JUGADOR(Nickname)
@@ -38,24 +59,65 @@ FOREIGN KEY (Nickname) REFERENCES JUGADOR(Nickname)
 
 --INSERT INTO SESION VALUES( );
 
+CREATE TABLE ROL(
+
+ID_Rol int IDENTITY PRIMARY KEY,
+Nombre_Rol varchar(30)
+);
+
+--INSERT INTO ROL VALUES( );
+
+
 CREATE TABLE HEROE(
 
 ID_Heroe int IDENTITY PRIMARY KEY NOT NULL,
-Nombre varchar(20),
-Rol varchar(20)
+Nombre_Heroe varchar(20),
+ID_Rol int
+
+FOREIGN KEY (ID_Rol) REFERENCES ROL(ID_Rol)
 
 );
 
 
 -- INSERT INTO HEROE VALUES ( );
 
+
 CREATE TABLE HABILIDADES (
 
-Nombre varchar(30) PRIMARY KEY,
+Nombre_Habilidad varchar(30) PRIMARY KEY,
 Daño int,
+NRO_Habilidad int,
 ID_Heroe int
 
 FOREIGN KEY (ID_Heroe) REFERENCES HEROE(ID_Heroe)
 );
 
 -- INSERT INTO HABILIDADES VALUES ( ); 
+
+
+CREATE TABLE PARTIDA_SESION(
+
+PRIMARY KEY(ID_Partida, ID_Sesion),
+ID_Partida int,
+ID_Sesion int NOT NULL,
+ID_Heroe int,
+EQUIPO_NRO int
+
+FOREIGN KEY (ID_Partida) REFERENCES PARTIDA(ID_Partida),
+FOREIGN KEY (ID_Sesion) REFERENCES SESION(ID_Sesion),
+FOREIGN KEY (ID_Heroe) REFERENCES HEROE(ID_Heroe)
+
+);
+
+--INSERT INTO PARTIDA_SESION( );
+
+CREATE TABLE GANADORES(
+
+PRIMARY KEY (ID_Partida, EQUIPO_NRO),
+ID_Partida int NOT NULL,
+EQUIPO_NRO int
+
+FOREIGN KEY (ID_Partida) REFERENCES PARTIDA(ID_Partida),
+FOREIGN KEY (EQUIPO_NRO) REFERENCES PARTIDA_SESION(EQUIPO_NRO) --There are no primary or candidate keys in the referenced table 'PARTIDA_SESION' that match the referencing column list in the foreign key 'FK__GANADORES__EQUIP__7B5B524B'.
+
+);
